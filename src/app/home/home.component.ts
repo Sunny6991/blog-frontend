1ce8c9b9b3789725c29f991aa54fe42e4fe0d489
sunny kumar <snykmr94@gmail.com>
2019-03-06 09:42:54 +0530

    my first-commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+//this is a by default statement
+import { Component, OnInit } from '@angular/core';
+//@ means decorator in angular
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
